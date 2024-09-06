e6cf94b5ae1d58a703fcd47c4c1b92a0153cfc81
abdlrhman08 <abdelrahmanhamada65@gmail.com>
2024-09-06 03:09:36 +0300

    initial stage of the job queue filling the queue with a loop

--- master/src/main.rs
+++ master/src/main.rs
@@ -1,19 +1,39 @@
 use tokio::task;
 use tonic::transport::Server;
 
+use std::sync::{Arc, Mutex};
+use std::time::Duration;
+
 use master::client;
-use net_interface::interface::test_net_server;
-use net_interface::TestNetServer;
+use net_interface::interface::{test_net_server, Job};
+use net_interface::{JobQueue, TestNetServer};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let job_queue = Arc::new(Mutex::new(JobQueue::new()));
+
     let address = "127.0.0.1:5001".parse()?;
-    let grpc_server = TestNetServer::new();
+    let grpc_server = TestNetServer::new(Arc::clone(&job_queue));
 
     // start the client interface on another thread since it is not the main
     // objective
     let client_thread = task::spawn(async {
         client::start_server().await;
+    });
+
+    let cloned_queue = Arc::clone(&job_queue);
+    let push_job_thread = std::thread::spawn(move || {
+        let mut t = 0;
+        loop {
+            println!("thread running");
+            t += 1;
+            let job_queue = &mut *cloned_queue.lock().unwrap();
+            let job = Job {
+                payload: format!("Job number {}", t),
+            };
+            job_queue.queue_job(job);
+            std::thread::sleep(Duration::from_secs(5));
+        }
     });
 
     Server::builder()
--- net_interface/src/lib.rs
+++ net_interface/src/lib.rs
@@ -2,27 +2,45 @@ pub mod interface {
     tonic::include_proto!("mainservice");
 }
 
+// we may need the tokio mutex, let's see how will this go
+use std::sync::{Arc, Mutex};
+
 use interface::test_net_server::TestNet;
 use interface::{Job, Sentinel};
 use tonic::{Request, Response, Status};
 
+pub struct JobQueue(Vec<Job>);
+
+impl JobQueue {
+    pub fn new() -> Self {
+        JobQueue(vec![])
+    }
+
+    pub fn queue_job(&mut self, job: Job) {
+        self.0.push(job);
+    }
+
+    pub fn get_upcoming(&mut self) -> Job {
+        self.0.remove(0)
+    }
+}
+
 pub struct TestNetServer {
-    job_queue: Vec<Job>,
+    job_queue: Arc<Mutex<JobQueue>>,
 }
 
 impl TestNetServer {
-    pub fn new() -> Self {
-        Self { job_queue: vec![] }
+    pub fn new(job_queue: Arc<Mutex<JobQueue>>) -> Self {
+        Self { job_queue }
     }
 }
 
 #[tonic::async_trait]
 impl TestNet for TestNetServer {
-    async fn pull_job(&self, request: Request<Sentinel>) -> Result<Response<Job>, Status> {
-        let job = Job {
-            payload: "i  pulled job from the queue".to_string(),
-        };
-
-        Ok(Response::new(job))
+    async fn pull_job(&self, _: Request<Sentinel>) -> Result<Response<Job>, Status> {
+        // i need help
+        let job_queue = &mut *self.job_queue.lock().unwrap();
+        let upcoming_job = job_queue.get_upcoming();
+        Ok(Response::new(upcoming_job))
     }
 }

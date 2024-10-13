4eb83b9cad9fe04d02fb6fe48adb24ba790bdbbc
Abdelrahman Hamada <69439342+abdlrhman08@users.noreply.github.com>
2024-10-13 17:10:24 +0300

    Modal and logs (#2)
    * first
    
    * Logs and Modal design
    
    * fixed stash
    
    ---------
    
    Co-authored-by: fady adel <fadya4265@gmail.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';  // Import the "Add" icon
 import { faRocket } from '@fortawesome/free-solid-svg-icons';
 import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import { useState, useEffect } from 'react';
 
 
@@ -41,12 +43,6 @@ export function DashboardSection()
         <div className='dashboard'>
           Dashboard
         </div>
-        {/* <div className='data'>
-          <button className='add'>
-            <FontAwesomeIcon icon={faPlus} />
-          </button>
-          <div className="notes">add notes</div>
-        </div> */}
       </div>
     </div>
   </>
@@ -56,25 +52,35 @@ export function DashboardSection()
 export function AddSection()
 {
   const [projects, setProjects] = useState({});
-
+  const [isOpenedModal, setIsOpenedModal] = useState(false);
+  const [isOpenedLogs, setIsOpenedLogs] = useState([]);
   useEffect(() => {
     fetch("/api/projects")
       .then((response) => response.json())
-      .then((json) => setProjects({ ...json }))
+      .then((json) => 
+        {setProjects({ ...json });
+         setIsOpenedLogs(Array(json.projects.length).fill(false));})
       .catch((e) => console.log(e));
   }, []);
 
+  function handleLogs(index)
+  {
+    let next_isOpenedLogs = [...isOpenedLogs];
+    next_isOpenedLogs[index] = !next_isOpenedLogs[index];
+    setIsOpenedLogs(next_isOpenedLogs);
+  }
   return (
   <>
   <div className='add-section'>
     <div className='container'>
+      <Modal isOpen={isOpenedModal} close={() =>{setIsOpenedModal(false)}}/>
       <div className='projects'>
-        {Object.keys(projects).map((key) => (
-          <div className='item'>
+        {Object.keys(projects).map((key, index) => (
+          <div key={key} className='item'>
             <div className='info'>
               <div className='data'>
                 <p>{projects[key].name}</p>
-                //TODO!
+                {/* TODO! */}
                 <p>c23dsa</p>
               </div>
               <div className='logo'>
@@ -82,15 +88,19 @@ export function AddSection()
               </div>
             </div>
             <div className='result'>
+              <button className='show-logs' onClick={() =>{handleLogs(index);}}>
+                {isOpenedLogs[index]? (<FontAwesomeIcon icon={faChevronUp} />) : (<FontAwesomeIcon icon={faChevronDown} />)}  
+              </button>
               <FontAwesomeIcon icon={faCheck} style={{ color: 'green' }} />
               {/* <FontAwesomeIcon icon={faTimes} style={{ color: 'red' }} /> Wrong */}
             </div>
+            {isOpenedLogs[index] && <Logs />}
           </div>
         ))}
       </div>
       <div className='add'>
         <div className='add-items'>
-          <button>
+          <button onClick={() =>{setIsOpenedModal(true)}}>
             <FontAwesomeIcon icon={faPlus} />
           </button>
           <div>add projects</div>
@@ -108,6 +118,41 @@ export function AddSection()
 );
 }
 
+export function Modal({isOpen , close})
+{
+  if(!isOpen)
+  {
+    return null;
+  }
+  return (
+  <>
+  <div className='modal-overlay'>
+    <div className='modal'>
+      <button className='close-modal' onClick={close}>
+        <FontAwesomeIcon icon={faTimes} />
+      </button>
+      <p className='text'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus ipsam suscipit recusandae adipisci fugit laborum repudiandae, temporibus illo eligendi iure officia quibusdam laudantium at ratione libero est corrupti veniam ab.
+      </p>
+    </div>
+  </div>
+  </>);
+}
+export function Logs()
+{
+  return (
+  <>
+  <div className='logs'>
+    <p>Log1</p>
+    <p>Log2</p>
+    <p>Log3</p>
+    <p>Log4</p>
+  </div>
+  </>);
+}
+
+
+
 export function FooterSection()
 {
   return (
@@ -124,20 +169,3 @@ export function FooterSection()
 
 export default App;
 
-
-
-
-{/* <div className="header-section">
-    <div className="container">
-      <div className='logo'>
-        <FontAwesomeIcon icon={faRocket} size="2x" />
-        <span>Test net</span>
-      </div>
-      <div className='data'>
-        <button className='add'>
-          <FontAwesomeIcon icon={faPlus} />
-        </button>
-        <div className="notes">add notes</div>
-      </div>
-    </div>
-  </div> */}
